d70cc42cde3d0924fcc84de030eb64e66165da8a
deilsonferreira <106764067+deilsonferreira@users.noreply.github.com>
2023-04-27 15:31:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let student = prompt("Qual o nome do(a) aluna(a?")
+
+let nota1 = prompt("Qual a sua nota da primeira prova?")
+let nota2 = prompt("Qual a sua nota da segunda prova?")
+
+function mediaProvas (num1,num2) {
+    var media = (nota1+nota2) / 2;
+}
+
+if(media >=7)
+if(media==10)
+alert("Aprovado")
+else
+alert("Reprovado")
